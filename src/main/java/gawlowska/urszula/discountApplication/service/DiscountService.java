d348e071e42b81fc42d7607e0d15d3df173978b6
Urszula Gawłowska <u.j.gawlowska@gmail.com>
2019-05-08 14:35:13 +0200

    improvements added to calculations

--- src/main/java/gawlowska/urszula/discountApplication/service/DiscountService.java
+++ src/main/java/gawlowska/urszula/discountApplication/service/DiscountService.java
@@ -3,9 +3,8 @@ package gawlowska.urszula.discountApplication.service;
 import gawlowska.urszula.discountApplication.model.Discount;
 import gawlowska.urszula.discountApplication.model.Product;
 
-import java.util.List;
 import java.util.Map;
 
 public interface DiscountService {
-    Map<Product, Discount> getProductsWithDiscounts(List<Product> products, double discount);
+    Map<Product, Discount> getProductsWithDiscounts();
 }
--- src/main/java/gawlowska/urszula/discountApplication/service/impl/DiscountServiceImpl.java
+++ src/main/java/gawlowska/urszula/discountApplication/service/impl/DiscountServiceImpl.java
@@ -4,27 +4,72 @@ import gawlowska.urszula.discountApplication.model.Discount;
 import gawlowska.urszula.discountApplication.model.Product;
 import gawlowska.urszula.discountApplication.service.DiscountService;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DiscountServiceImpl implements DiscountService {
+    private List<Product> allProducts;
+    private double totalDiscountAmount;
+
+    public DiscountServiceImpl(List<Product> allProducts, double totalDiscountAmount) {
+        this.allProducts = allProducts;
+        this.totalDiscountAmount = totalDiscountAmount;
+    }
+
     @Override
-    public Map<Product, Discount> getProductsWithDiscounts(List<Product> products, double discount) {
+    public Map<Product, Discount> getProductsWithDiscounts() {
         Map<Product, Discount> productsWithDiscounts = new HashMap<>();
-        double unitDiscount = calculateDiscountPerOneUnit(products, discount);
-        for (Product product : products) {
-            productsWithDiscounts.put(product, new Discount(calculateDiscountForProduct(product, unitDiscount)));
+        if (validateData()) {
+            double unitDiscount = calculateDiscountPerOneUnit();
+            for (Product product : allProducts) {
+                productsWithDiscounts.put(product, new Discount(calculateDiscountForProduct(product, unitDiscount)));
+            }
+
+            for (int i = 0; i < allProducts.size(); i++) {
+                if (allProducts.size() > 1 && i == allProducts.size() - 1) {
+                    productsWithDiscounts.put(allProducts.get(i),
+                            new Discount(calculateDiscountForLastProduct(productsWithDiscounts)));
+                } else {
+                    productsWithDiscounts.put(allProducts.get(i),
+                            new Discount(calculateDiscountForProduct(allProducts.get(i), unitDiscount)));
+                }
+            }
         }
-        return null;
+        return productsWithDiscounts;
     }
 
     private double calculateDiscountForProduct(Product product, double discountPerOneUnit) {
-        return product.getPrice() * discountPerOneUnit;
+        double discountForProduct = product.getPrice() * discountPerOneUnit;
+        BigDecimal discountForProductRounded = new BigDecimal(discountForProduct).setScale(2, BigDecimal.ROUND_DOWN);
+        return discountForProductRounded.doubleValue();
     }
 
-    private double calculateDiscountPerOneUnit(List<Product> products, double discount) {
-        double sum = products.stream().mapToDouble(Product::getPrice).sum();
-        return discount/sum;
+    private double calculateDiscountPerOneUnit() {
+        double sum = allProducts.stream().mapToDouble(Product::getPrice).sum();
+        return totalDiscountAmount / sum;
+    }
+
+    private boolean validateData() {
+        boolean dataAreValid = true;
+        boolean productsAreValid = true;
+        boolean discountIsValid = true;
+
+        if (!(totalDiscountAmount >= 0)) {
+            discountIsValid = false;
+        }
+        if (allProducts.size() == 0 || allProducts.size() > 5) {
+            productsAreValid = false;
+        }
+        if (!productsAreValid || !discountIsValid) {
+            dataAreValid = false;
+        }
+        return dataAreValid;
+    }
+
+    private double calculateDiscountForLastProduct(Map<Product, Discount> productDiscountMap) {
+        double grantedDiscount = productDiscountMap.entrySet().stream().mapToDouble(entry -> entry.getValue().getAmount()).sum();
+        return totalDiscountAmount - grantedDiscount;
     }
 }
