c3cbf1921ab815ee078ed30d9db6e06b191c45e1
Urszula Gawłowska <u.j.gawlowska@gmail.com>
2019-05-08 10:37:59 +0200

    project initialization

---
+++ src/main/java/gawlowska/urszula/discoutApplication/DiscoutApplication.java
@@ -0,0 +1,13 @@
+package gawlowska.urszula.discoutApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gawlowska/urszula/discoutApplication/DiscoutApplicationTests.java
@@ -0,0 +1,16 @@
+package gawlowska.urszula.discoutApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
